d5191a115afd1b47e5cc777e3ad11e6c173e2bbd
Vanama Nikhith <133476937+Nikhith-Vanama@users.noreply.github.com>
2025-06-30 18:56:48 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,12 @@
+class Question {
+    String question, optionA, optionB, optionC, optionD, correct;
+
+    public Question(String question, String a, String b, String c, String d, String correct) {
+        this.question = question;
+        this.optionA = a;
+        this.optionB = b;
+        this.optionC = c;
+        this.optionD = d;
+        this.correct = correct;
+    }
+}
